fb24c06bfde66d35180b54ea484cb5c2cfd684fa
Shivp436 <mpshivanandpatil2002@gmail.com>
2024-07-24 15:44:09 +0530

    Testing Husky

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+const { jsx } = require('react/jsx-runtime');
+
+module.exports = {
+  semi: true,
+  trailingComma: 'es5',
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  tabWidth: 2,
+  endOfLine: 'auto'
+}
